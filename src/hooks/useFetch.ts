0ec7b46cc1e374c03aae58701fab9de9d4dc55a4
flowl <florian.padel@epita.fr>
2023-10-10 13:19:44 +0200

    feat: add useFetch to handle fetching states

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = <T>(promise: Promise<T>) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<null | Error>(null);
+  const [data, setData] = useState<T | null>(null);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setIsLoading(true);
+      promise
+        .then((res) => {
+          setData(res)
+        })
+        .catch((err) => setError(err))
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, []);
+
+  return {
+    isLoading,
+    isError: !!error,
+    error: error,
+    data: data
+  };
+};
