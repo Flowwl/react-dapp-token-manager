ace0ac8da6aa07b6c7a48090c184f3dc18b7fc8e
flowl <florian.padel@epita.fr>
2023-10-10 15:36:01 +0200

    feat: add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import './App.css';
 import WalletButton from "./components/WalletButton.tsx";
-
 function App() {
   return (
-    <div>
-      App
+    <div className="h-screen w-full flex flex-col items-center justify-center">
       <WalletButton/>
     </div>
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import RPCPublicClientContext from "./contexts/RPCPublicClientContext.tsx";
+import "./globals.scss"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
