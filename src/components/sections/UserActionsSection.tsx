3abbfebc82b65558352229f817de47cb01491510
flowl <florian.padel@epita.fr>
2023-10-11 14:25:06 +0200

    feat: add approveTo

--- src/components/sections/UserActionsSection.tsx
+++ src/components/sections/UserActionsSection.tsx
@@ -7,6 +7,7 @@ import { useTransferTo } from "../../hooks/useTransferTo.ts";
 import Spinner from "../atoms/Spinner.tsx";
 import { useMint } from "../../hooks/useMint.ts";
 import { useBurn } from "../../hooks/useBurn.ts";
+import { useApproveTo } from "../../hooks/useApproveTo.ts";
 
 interface UserActionsSectionProps {
   className?: string;
@@ -20,14 +21,16 @@ const UserActionsSection: FC<UserActionsSectionProps> = ({ className }) => {
   const [transferValue, setTransferValue] = useState<string>("0");
   const [mintValue, setMintValue] = useState<string>("0");
   const [burnValue, setBurnValue] = useState<string>("0");
+  const [approveTo, setApproveTo] = useState<string>("");
+  const [approveValue, setApproveValue] = useState<string>("0");
 
   const { data: totalSupply } = useGetTotalSupply(selectedToken);
   const { data: userBalance, isLoading: isBalanceLoading } = useGetUserBalanceByToken(address);
   const { transfer } = useTransferTo();
   const { mint } = useMint();
   const { burn } = useBurn();
+  const { approve } = useApproveTo();
 
-  console.log(mintValue, burnValue)
   return (
     <div className={cx(className)}>
       <div>Total Supply: {totalSupply || 0}</div>
@@ -51,16 +54,19 @@ const UserActionsSection: FC<UserActionsSectionProps> = ({ className }) => {
           transfer(transferTo, transferValue)
         }}>
           <p>Send transaction</p>
-          <input type="text" placeholder="0x..." value={transferTo} onChange={(e) => setTransferTo(e.target.value)} pattern="0x[0-9a-f]*"/>
-          <input type="number" placeholder="1.3" value={transferValue} onChange={(e) => setTransferValue(e.target.value)} />
+          <input type="text" placeholder="0x..." onChange={(e) => setTransferTo(e.target.value)} pattern="0x[0-9a-f]*"/>
+          <input type="number" placeholder="1.3" onChange={(e) => setTransferValue(e.target.value)} />
           <button type={"submit"}>Send</button>
         </form>
-        {/* FIXME */}
-        <div className="flex justify-between gap-5">
+        <form className="flex justify-between gap-5" onSubmit={(e) => {
+          e.preventDefault();
+          approve(approveTo, approveValue)
+        }}>
           <p>Approve address</p>
-          <input type="text" placeholder="0x..."/>
-          <button>Approve</button>
-        </div>
+          <input type="text" placeholder="0x..." onChange={(e) => setApproveTo(e.target.value)}/>
+          <input type="number" step="0.000001" placeholder="1234" onChange={(e) => setApproveValue(e.target.value)}/>
+          <button type="submit">Approve</button>
+        </form>
         {/* FIXME */}
         <div className="flex justify-between gap-5">
           <p>Transfer from</p>
---
+++ src/hooks/useApproveTo.ts
@@ -0,0 +1,34 @@
+import { useChainContext, useConnectedWalletContext } from "../contexts";
+import { useFetch } from "./useFetch.ts";
+import { useState } from "react";
+import { computeFloatToBigInt } from "../utils";
+import { TOKENS } from "../constants/tokens.ts";
+
+export const useApproveTo = () => {
+  const { walletClientActions, selectedToken, publicClientActions, tokenDecimals } = useChainContext();
+  const { address } = useConnectedWalletContext();
+  const [value, setValue] = useState("0")
+  const [to, setTo] = useState<string>("")
+  const promise = async () => {
+    const { request } = await publicClientActions.simulateContract({
+      account: address,
+      address: TOKENS[selectedToken].address,
+      abi: TOKENS[selectedToken].abi,
+      functionName: 'approve',
+      args: [to, computeFloatToBigInt(parseFloat(value), tokenDecimals)]
+    });
+    return walletClientActions.writeContract(request);
+  };
+
+  const approve = (to: string, value: string) => {
+    setTo(to);
+    setValue(value);
+    fetchMethods.setEnabled(true)
+  }
+
+  const fetchMethods = useFetch(async () => promise(), { isEnabled: false })
+  return {
+    approve,
+    ...fetchMethods
+  }
+};
