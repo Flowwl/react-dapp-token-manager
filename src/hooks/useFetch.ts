005fe87ce1f1bc2a68853ab0972b2b5c0a471aa1
flowl <florian.padel@epita.fr>
2023-10-11 11:06:43 +0200

    feat: add a retry in useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,9 +4,10 @@ export const useFetch = <T>(promise: Promise<T>) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<null | Error>(null);
   const [data, setData] = useState<T | null>(null);
+  const [retry, setRetry] = useState(true)
 
   useEffect(() => {
-    if (!isLoading) {
+    if (!isLoading && retry) {
       setIsLoading(true);
       promise
         .then((res) => {
@@ -15,6 +16,7 @@ export const useFetch = <T>(promise: Promise<T>) => {
         .catch((err) => setError(err))
         .finally(() => {
           setIsLoading(false);
+          setRetry(false);
         });
     }
   }, [promise]);
