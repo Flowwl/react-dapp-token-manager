f0ce261f20f9ebdcaa57c819f8e1a4ca929a3ef4
flowl <florian.padel@epita.fr>
2023-10-13 09:39:36 +0200

    fix: add OwnershipSection

---
+++ src/components/sections/OwnershipSection.tsx
@@ -0,0 +1,48 @@
+import { FC, useState } from 'react';
+import cx from "classnames";
+import Input from "../atoms/Input.tsx";
+import Button from "../atoms/Button.tsx";
+import { useTransferOwnership } from "../../hooks/useTransferOwnership.ts";
+import { useRenounceOwnership } from "../../hooks/useRenounceOwnership.ts";
+import { useGetOwner } from "../../hooks/useGetOwner.ts";
+import { useChainContext, useConnectedWalletContext } from "../../contexts";
+
+interface OwnershipSectionProps {
+  className?: string;
+}
+
+const OwnershipSection: FC<OwnershipSectionProps> = ({ className }) => {
+  const { selectedToken } = useChainContext();
+  const { account } = useConnectedWalletContext();
+  const { data: owner } = useGetOwner(selectedToken, { isEnabled: true  });
+  const [transferOwnershipTo, setTransferOwnershipTo] = useState<string>("");
+  const { transferOwnership } = useTransferOwnership();
+  const { renounceOwnership } = useRenounceOwnership();
+
+
+  const isOwner = owner === account;
+  if (! isOwner) { return null; }
+  return (
+    <>
+      <form className={cx(className)} onSubmit={(e) => {
+        e.preventDefault();
+        transferOwnership(transferOwnershipTo);
+      }}>
+        <p>Transfer Ownership to</p>
+        <div className="flex gap-4">
+          <Input type="text" label="To" onChange={(e) => setTransferOwnershipTo(e.target.value)}/>
+        </div>
+        <Button type="submit">Transfer Ownership</Button>
+      </form>
+      <form className={className} onSubmit={(e) => {
+        e.preventDefault();
+        renounceOwnership();
+      }}>
+        <p>Renounce Ownership</p>
+        <Button color="red" type="submit">Renounce Ownership</Button>
+      </form>
+    </>
+  );
+};
+
+export default OwnershipSection;
