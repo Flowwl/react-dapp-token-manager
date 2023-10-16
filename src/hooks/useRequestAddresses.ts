a3e3be1948497c30d4efb4748c42d004dfa8ee67
flowl <florian.padel@epita.fr>
2023-10-16 16:07:02 +0200

    fix: requestAddresses bug

--- src/hooks/useRequestAddresses.ts
+++ src/hooks/useRequestAddresses.ts
@@ -1,11 +1,18 @@
 import { useChainContext } from "../contexts";
 import { FetchOptions, useFetch } from "./useFetch.ts";
 import { RequestAddressesReturnType } from "viem";
+import { HexString } from "../types";
 
 export const useRequestAddresses = (opts: Partial<FetchOptions<RequestAddressesReturnType>> = {}) => {
   const { walletClientActions} = useChainContext();
-  const promise = async () => {
-    return walletClientActions.requestAddresses();
+  const promise = async (): Promise<Array<HexString>> => {
+    try {
+      const addresses = await walletClientActions.request({ method: "eth_requestAccounts"});
+      return addresses || [];
+    } catch (error) {
+      const addresses = await window.ethereum?.request({ method: "eth_requestAccounts" });
+      return addresses || [];
+    }
   };
   const requestAddresses = () => {
     fetchMethods.refetch();
