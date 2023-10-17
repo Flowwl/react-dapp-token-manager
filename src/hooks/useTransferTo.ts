2dc401afe3b829c8eae1f21dc3e85168f2c08051
flowl <florian.padel@epita.fr>
2023-10-17 10:30:22 +0200

    fix: useTransferTo

--- src/hooks/useTransferTo.ts
+++ src/hooks/useTransferTo.ts
@@ -3,26 +3,21 @@ import { FetchOptions, useFetch } from "./useFetch.ts";
 import { useState } from "react";
 import { computeFloatToBigInt } from "../utils";
 import { TOKENS } from "../constants/tokens.ts";
-import { assertAbiExists, assertAddressExists } from "../asserts";
+import { assertIsHexString } from "../asserts";
 
 export const useTransferTo = (opts: Partial<FetchOptions<boolean>> = {}) => {
-  const { walletClientActions, selectedToken, publicClientActions, tokenDecimals } = useChainContext();
+  const { walletClientActions, selectedToken, tokenDecimals } = useChainContext();
   const { account } = useConnectedWalletContext();
   const [value, setValue] = useState("0");
   const [to, setTo] = useState<string>("");
   const promise = async () => {
-    const address = TOKENS[selectedToken]?.address;
-    const abi = TOKENS[selectedToken]?.abi;
-    assertAddressExists(address);
-    assertAbiExists(abi);
-    const { request } = await publicClientActions.simulateContract({
+    assertIsHexString(to)
+    return walletClientActions.sendTransaction({
+      chain: TOKENS[selectedToken].chain,
       account,
-      address,
-      abi: abi,
-      functionName: 'transfer',
-      args: [to, computeFloatToBigInt(parseFloat(value), tokenDecimals)]
-    });
-    return walletClientActions.writeContract(request) as unknown as boolean;
+      to: to,
+      value: computeFloatToBigInt(parseFloat(value), tokenDecimals)
+    }) as unknown as boolean;
   };
 
   const transfer = (to: string, value: string) => {
