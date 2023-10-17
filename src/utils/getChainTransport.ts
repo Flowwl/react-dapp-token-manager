bac10e97f282a629b6b7c7981ace153cf5878b1b
flowl <florian.padel@epita.fr>
2023-10-17 09:04:57 +0200

    fix: add fallback in transport

--- src/utils/getChainTransport.ts
+++ src/utils/getChainTransport.ts
@@ -8,5 +8,20 @@ export const getChainTransport = (chain: Chain): ClientConfig => {
     throw new Error("MetaMask or another web3 wallet is not installed. Please install one to proceed.");
   }
   const alchemy = http(`https://polygon-mumbai.g.alchemy.com/v2/${ENV_CONFIG.ALCHEMY_API_KEY}`);
-  return { chain, transport: fallback([alchemy, custom(window.ethereum)], { rank: true }) }
+  return { chain, transport: fallback([
+    custom(
+      {
+        async request({ method, params }) {
+          try {
+            const response = await alchemy({ chain }).request({ method, params });
+            return response;
+          }
+          catch (e) {
+            const response = await window.ethereum?.request({ method, params });
+            return response;
+          }
+        }
+      }),
+      custom(window.ethereum)
+    ], { retryCount: 1 }) }
 }
