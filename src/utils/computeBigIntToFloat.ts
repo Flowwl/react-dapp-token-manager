997dd49369c463c6021c171c26d6d15dbec4c8ad
flowl <florian.padel@epita.fr>
2023-10-23 13:31:46 +0200

    fix: computeBigIntToFloat.ts

--- src/utils/computeBigIntToFloat.ts
+++ src/utils/computeBigIntToFloat.ts
@@ -1,4 +1,3 @@
 export const computeBigIntToFloat = (value: bigint | number, decimals: bigint | number) => {
-  const res = Number(BigInt(value) * 100n / (BigInt(10) ** BigInt(decimals))) / 100;
-  return parseFloat(res.toFixed(5))
+  return Number(value) / 10 ** Number(decimals);
 }
